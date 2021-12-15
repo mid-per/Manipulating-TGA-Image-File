6f05b45318f02a3e1d612ebd2100f5a8ba42f0ad
mid-per <90703846+mid-per@users.noreply.github.com>
2021-12-15 22:42:50 +0700

    Add files via upload

---
+++ tga_reader.c
@@ -0,0 +1,89 @@
+#include <stdio.h> // fopen, fread, ...
+#include <stdlib.h> // malloc, free, ...
+#include <string.h> // for memcmp
+#include <stdint.h> // for uint8_t
+
+typedef uint8_t ubyte;
+
+struct Pixel_arr {
+    ubyte* data;
+    size_t width;
+    size_t height;
+    ubyte format;
+};
+
+typedef struct Pixel_arr PixelArray;
+
+int main()
+{
+    char c; // 1 byte
+    unsigned char u; // 1 byte
+
+    FILE* fp = fopen("earth.tga", "rb");
+    FILE* outfile = NULL;
+    if (fp == NULL) {
+        fprintf(stderr, "cannot open file\n");
+        return -1;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    size_t fsize = ftell(fp);
+    // errno;
+
+    fseek(fp, 0, SEEK_SET);
+    ubyte* data = (ubyte*)malloc(fsize);
+
+    const ubyte tga_sig[7] = {0, 0, 2, 0, 0, 0, 0};
+    const size_t tga_header_size = 18;
+    PixelArray pix;
+    pix.data = NULL;
+
+    int res = fread(data, 1, fsize, fp);
+    if (res < fsize) {
+        fprintf(stderr, "invalid tga file\n");
+        goto done;
+    }
+
+    res = memcmp(tga_sig, data, sizeof(tga_sig));
+    if (res != 0) {
+        fprintf(stderr, "invalid tga file\n");
+        goto done;
+    }
+
+    pix.width = data[12] + (data[13] << 8);
+    pix.height = data[14] + (data[15] << 8);
+    pix.format = data[16];
+    if (pix.format != 24) {
+        fprintf(stderr, "unsupported tga format\n");
+        goto done;
+    }
+
+    size_t image_size = pix.width * pix.height * pix.format / 8;
+    if ((tga_header_size + image_size) > fsize) {
+        fprintf(stderr, "invalid tga file size\n");
+        goto done;
+    }
+
+    pix.data = (ubyte*)malloc(image_size);
+    memcpy(pix.data, data + 18, image_size);
+
+    ubyte buffer[18] = {};
+    memcpy(buffer, tga_sig, sizeof(tga_sig));
+    buffer[12] = pix.width;
+    buffer[13] = pix.width >> 8;
+    buffer[14] = pix.height;
+    buffer[15] = pix.height >> 8;
+    buffer[16] = pix.format;
+
+    outfile = fopen("done.tga", "wb");
+    fwrite(buffer, 1, sizeof(buffer), outfile);
+    fwrite(pix.data, 1, image_size, outfile);
+
+done:
+    fclose(outfile);
+
+    free(pix.data);
+    free(data);
+    fclose(fp);
+    return 0;
+}
